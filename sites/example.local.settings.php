513e74a68fc1b56e5468cd373613041ef3bf1993
Shivam Agarwal <mr.shivamagarwal@gmail.com>
2015-04-02 16:38:10 +0530

    local setting.php

--- sites/example.local.settings.php
+++ sites/example.local.settings.php
@@ -5,10 +5,10 @@
  * Local development override configuration feature.
  *
  * To activate this feature, copy and rename it such that its path plus
- * filename is 'sites/example.com/settings.local.php', where example.com
+ * filename is 'sites/example.com/local.settings.php', where example.com
  * is the name of your site. Then, go to the bottom of
  * 'sites/example.com/settings.php' and uncomment the commented lines that
- * mention 'settings.local.php'.
+ * mention 'local.settings.php'.
  */
 
 /**
